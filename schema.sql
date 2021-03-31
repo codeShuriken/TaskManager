633cd9e8cf956e734c83223b521d0bb2d406daf0
Gowtham Vuppala <gowthamvuppala1@gmail.com>
2021-03-31 09:40:32 -0400

    Updated schema

--- schema.sql
+++ schema.sql
@@ -1,2 +1,30 @@
-use tasksmanager;
-update users set roles='ROLE_USER' where id=1;
+create database if not exists tasksManager;
+
+use tasksManager;
+
+drop table if exists tasks;
+drop table if exists users;
+
+create table `users` (
+	`id` bigint not null auto_increment,
+    `username` varchar(255) default null,
+    `email` varchar(255) default null,
+    `password` varchar(255) default null,
+    `roles` varchar(255) default null,
+    primary key (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tasks` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `start_date` datetime(6) DEFAULT NULL,
+  `end_date` datetime(6) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `severity` varchar(255) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  
+  KEY `FK_USER_idx` (`user_id`),
+  CONSTRAINT `FK_USER` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  ON UPDATE NO ACTION ON DELETE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
